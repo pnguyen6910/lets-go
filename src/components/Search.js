85d0d34214374225394d9527505125290e0e086a
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-10 12:35:08 -0500

    Add components

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <form className="row g-3">
+
+            <div className="col-auto">
+
+                <input className="form-control" />
+            </div>
+            <div className="col-auto">
+                <button type="submit" className="btn btn-primary mb-3">Go</button>
+            </div>
+        </form>
+    )
+}
+
+export default Search;
