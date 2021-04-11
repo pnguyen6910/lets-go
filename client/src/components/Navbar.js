eb3241460e49f58f21fd60ac83fe5adbcc7d2489
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-11 18:21:12 -0500

    Map

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Welcome</a>
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Welcome</a>
                 </div>
             </nav>
         </div>
