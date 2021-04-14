a97df4384144ed95661b3c31f49415cb973a0517
Ryan <ryanpburnett@yahoo.com>
2021-04-13 19:49:59 -0500

    more seed data

--- seeds.js
+++ seeds.js
@@ -16,5 +16,23 @@ const seeds = [
         latitude: 41.8796,
         longitude: -87.6237,
         description: "Vita brevis, Ars longa."
-    }
+    },
+    {
+        name: "Cloud Gate",
+        latitude: 41.8827,
+        longitude: -87.6233,
+        description: "Otherwise known as The Bean."
+    },
+    {
+        name: "Navy Pier",
+        latitude: 41.8919,
+        longitude: -87.6051,
+        description: "Popular tourist attraction.  Go ride the Ferris Wheel!"
+    },
+    {
+        name: "Maria's Packaged Goods and Community Bar",
+        latitude: 41.8380992726567,
+        longitude: -87.65110138119557,
+        description: "Best bar in Bridgeport.  Tell 'em Ryan sent you."
+    },
 ]
