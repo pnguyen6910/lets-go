ce1fa504326aa1b8504bfb6e08fb4e399429196b
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-15 18:51:27 -0500

    updating

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -6,8 +6,8 @@ function Search() {
         <form className="row g-3">
 
             <div className="col-auto">
-
                 <input className="form-control" />
+
             </div>
             <div className="col-auto">
                 <button type="submit" className="btn btn-primary mb-3">Go</button>
