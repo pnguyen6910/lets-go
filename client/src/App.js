738b032d6da6b08c71dfc293cdaa37e4d6a08484
SmoczyBaron <73004051+SmoczyBaron@users.noreply.github.com>
2021-04-16 02:33:27 -0500

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import { StyleSheet, Text, Viev } from "react-native";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import './App.css';
+import { render } from 'react-dom';
 
+export default class App extends React.Component {
+
+  constructor(progs) {
+    super(progs);
+    this.state = { isLoading : true };
+  }
+
+}
+
+componentDidMount() ;{
+  return fetch()
+    .then((response) => response.json()) 
+    .then((responseJson) => {
+      this.setState({
+      isLoading : false,
+      message : responseJson.message
+    })
+  })
+
+}
+
+
+
+  
+
+ render(); {  
+   return (
+     <View style ={styles.container}>
+     <Text>Open up App.js to start working on yout app!</Text>
+     </View>
+     );
+   }
+  
+  
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: 'afff',
+     alignItems: 'conter',
+     justifyContent: 'conter',
+
+   },
+ });
 
 
 // var myIcon = L.icon({
