f6542bd75e4117e90d33e0ae4e97870e71d1afe7
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-17 13:06:15 -0500

    Uninstall react-leaflet-search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Search from "react-leaflet-search"
+
 
 function Search() {
     return (
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import Search from '../components/Search'
 import './Home.css'
 import L from 'leaflet'
