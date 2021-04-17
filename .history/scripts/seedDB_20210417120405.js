95e3ac00e73a3dd5a47e275f598348a479dcce4b
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-04-17 12:28:49 -0500

    converting seed data

---
+++ .history/scripts/seedDB_20210417120405.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dailydoodles");
+
+const drawingsSeed = [
+    {
+        title: "Square",
+        body:
+            "Welcome to your first Drawing!",
+        date: new Date(Date.now()),
+        drawing:
+  }
+];
+
+const userSeed = {
+    username: 'admin',
+}
+
+const runSeeder = async () => {
+    try {
+        await db.User.remove({})
+        await db.Map.remove({})
+        const result = await db.Map.insertMany(mapsSeed, { raw: true })
+        const mapIds = result.map(point => point._id)
+        const finalUserData = {
+            ...userSeed,
+            drawings: drawingIds
+        }
+        const user = await db.User.create(finalUserData)
+        await db.Drawing.update({}, { user: user._id })
+    } catch (err) {
+        throw new err
+    }
+    process.exit()
+}
+
+runSeeder()
