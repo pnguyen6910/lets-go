6bdd507da1b5bc78484f3625aa86f2dd06b9c711
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-04-20 00:21:04 -0500

    seed attempt

--- .history/server_20210419230727.js
+++ .history/server_20210419230727.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -17,12 +16,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Send every other request to the react app
-//define any api routes before this runs
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
 
 // Start the API server
 app.listen(PORT, function () {
