3694afdaff487cc0721d7852a5cc34e088e5f4e7
Ryan <ryanpburnett@yahoo.com>
2021-04-22 21:09:52 -0500

    sidebar populating seed data

---
+++ client/src/components/Card.js
@@ -0,0 +1,36 @@
+import React,  { Component }  from 'react';
+import Axios from 'axios';
+import API from '../utils/API'
+import { DivOverlay } from 'leaflet';
+
+
+class Card extends Component {
+    state = {
+        markers: [],
+        lat: 41.875440756396515,
+        lng: -87.62412071228027,
+        marker: []
+    }
+
+    componentDidMount() {
+        API.getPoints()
+            .then(response => this.setState({ markers: response.data }))
+    }
+
+    render() {
+        
+        return (<div className="container-fluid">
+            {this.state.markers.map(marker => {
+                return (
+                    <div>
+                        <strong>{marker.location}</strong>
+                        <h6>{marker.details}</h6>
+                    </div>
+                )
+            })}
+        </div>)
+    }
+
+}
+
+export default Card;
