1e8f356a3fa8e05bdb675e343051175f71fb782f
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-22 21:39:17 -0500

    update

--- models/map.js
+++ models/map.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
 const mapSchema = new Schema({
     position: {
-        lat: Integer,
-        lng: Integer
+        lat: Schema.Types.Decimal128,
+        lng: Schema.Types.Decimal128,
     },
-    popup: String,
+    location: {
+        type: String,
+        required: "Please enter a name"
+    },
+    details: {
+        type: String,
+        required: "Please enter a description"
+    },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 })
-
 const Map = mongoose.model('Map', mapSchema)
-
 module.exports = Map
